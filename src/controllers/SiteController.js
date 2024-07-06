c106ec776c866ede3c817ab82292c91afcad7e38
PhamNgocSonTech <phamngocson7a1@gmail.com>
2024-07-06 09:54:56 +0700

    Change card component UI, read course from db

--- src/controllers/SiteController.js
+++ src/controllers/SiteController.js
@@ -1,4 +1,5 @@
 const CourseModel = require('../models/Course');
+const { mutipleMongooseToObject } = require('../utils/mongoose');
 
 const getCourse = async (req, res) => {
     try {
@@ -11,8 +12,9 @@ const getCourse = async (req, res) => {
 
 const Homepage = async (req, res) => {
     try {
-        const data = await CourseModel.find();
-        res.render('home', { courses: data });
+        const courses = await CourseModel.find({}).lean();
+        // const dataCourses = courses.map((course) => course.toObject());
+        res.render('home', { courses });
         // return res.status(200).json(data);
     } catch (error) {
         res.status(404).json(error);
